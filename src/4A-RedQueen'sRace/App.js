204b6bd9efd2080326f07e3d630d4d4ab23a5763
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-08-09 07:01:23 +0500

    Project 4A complete

--- src/4A-RedQueen'sRace/App.js
+++ src/4A-RedQueen'sRace/App.js
@@ -39,7 +39,7 @@ function Sky() {
       };
       
       var sceneryTimingForeground = {
-        duration: 12000,
+        duration: 6000,
         iterations: Infinity
       };
 
@@ -74,19 +74,39 @@ function Sky() {
       
       <div className="earth">
         <div className="alice">
-            <img className="alicesprite" ref={aliceSprite} src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png" alt=" " />
+            <img className="alicesprite"
+                 ref={aliceSprite}
+                 src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png"
+                 alt=" " />
         </div>
       </div>
       
-      <div className="scenery" id="foreground" ref={foreground}>
-        <img id="treefore" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3_small.png" alt=" "/>
+      <div className="scenery"
+           id="foreground"
+           ref={foreground}>
+        <img id="treefore"
+             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3_small.png"
+             alt=" "/>
       </div>
 
-      <div className="scenery background1" ref={background}>
-        <img className="pawn" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright_small.png" alt=" " />
-        <img className="pawn2" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright_small.png" alt=" " />
-        <img className="treeback" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1_small.png" alt=" " />
-        <img id="w_rook" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_small.png" srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook.png 2x" alt=" " />
+      <div className="scenery background1"
+           ref={background}>
+        <img className="pawn"
+             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright_small.png"
+             alt=" " />
+
+        <img className="pawn2"
+             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright_small.png"
+             alt=" " />
+
+        <img className="treeback"
+             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1_small.png"
+             alt=" " />
+
+        <img id="w_rook"
+             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_small.png"
+             srcSet="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook.png 2x"
+             alt=" " />
       </div>
     </div>
   );
