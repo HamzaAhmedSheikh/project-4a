1f7bfd04416f7e9bef451a40189fbe4d657c75ec
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-08-09 05:39:43 +0500

    mistake solved

--- src/4A-RedQueen'sRace/App.js
+++ src/4A-RedQueen'sRace/App.js
@@ -1,96 +1,94 @@
 import React, { useLayoutEffect, useRef } from 'react';
-
-import './style.css'
-
-function RedQueen() {
+import './style.css';
+function Sky() {
  
-    const aliceSprite = useRef(null);
-    const foreground = useRef(null);
-    const background = useRef(null);
-  
-    useLayoutEffect(() => {
-        var spriteFrames = [
-          { transform: 'translateY(0)' },
-          { transform: 'translateY(-100%)' }   
-        ];
-  
-        var alice = aliceSprite.current.animate(
-          spriteFrames, {
-            easing: 'steps(7, end)',
-            direction: "reverse",
-            duration: 500,
-            playbackRate: 1,
-            iterations: Infinity
-          });
-  
-        setInterval( function() {
-          if (alice.playbackRate > .4) {
-            alice.playbackRate -= .1;
-            adjustSceneryPlayback();
-          } 
-        }, 3000);
-  
-        var sceneryFrames =   [
-          { transform: 'translateX(100%)' },
-          { transform: 'translateX(-100%)' }   
-        ];
-        
-        var sceneryTimingBackground = {
-          duration: 36000,
+  const aliceSprite = useRef(null);
+  const foreground = useRef(null);
+  const background = useRef(null);
+
+  useLayoutEffect(() => {
+      var spriteFrames = [
+        { transform: 'translateY(0)' },
+        { transform: 'translateY(-100%)' }   
+      ];
+
+      var alice = aliceSprite.current.animate(
+        spriteFrames, {
+          easing: 'steps(7, end)',
+          direction: "reverse",
+          duration: 500,
+          playbackRate: 1,
           iterations: Infinity
-        };
-        
-        var sceneryTimingForeground = {
-          duration: 12000,
-          iterations: Infinity
-        };
-  
-        foreground.current.animate(sceneryFrames, sceneryTimingForeground);
-        background.current.animate(sceneryFrames, sceneryTimingBackground);
-  
-        var sceneries = [  ];
-  
-        var adjustSceneryPlayback = function() {
-          console.log(alice.playbackRate)
-          if (alice.playbackRate < .8) {
-            sceneries.forEach(function(anim) {
-              anim.playbackRate = alice.playbackRate/2 * -1;
-            });
-          }  
-        }
-        adjustSceneryPlayback();
-  
-        const goFaster = () => {
-          alice.playbackRate += 0.1;
+        });
+
+      setInterval( function() {
+        if (alice.playbackRate > .4) {
+          alice.playbackRate -= .1;
           adjustSceneryPlayback();
-        }
-    
-        window.addEventListener("click", goFaster);
-    })
+        } 
+      }, 3000);
+
+      var sceneryFrames =   [
+        { transform: 'translateX(100%)' },
+        { transform: 'translateX(-100%)' }   
+      ];
+      
+      var sceneryTimingBackground = {
+        duration: 36000,
+        iterations: Infinity
+      };
+      
+      var sceneryTimingForeground = {
+        duration: 12000,
+        iterations: Infinity
+      };
+
+      foreground.current.animate(sceneryFrames, sceneryTimingForeground);
+      background.current.animate(sceneryFrames, sceneryTimingBackground);
+
+      var sceneries = [  ];
+
+      var adjustSceneryPlayback = function() {
+        console.log(alice.playbackRate)
+        if (alice.playbackRate < .8) {
+          sceneries.forEach(function(anim) {
+            anim.playbackRate = alice.playbackRate/2 * -1;
+          });
+        }  
+      }
+      adjustSceneryPlayback();
+
+      const goFaster = () => {
+        alice.playbackRate += 0.1;
+        adjustSceneryPlayback();
+      }
   
-    
-    
-    return (
-      <div className="container">
-        <div className="sky"></div>
-        
-        <div className="earth">
-          <div className="alice">
-              <img className="alicesprite" ref={aliceSprite} src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png" alt=" " />
-          </div>
-        </div>
-        
-        <div className="scenery" id="foreground" ref={foreground}>
-          <img id="treefore" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3_small.png" alt=" "/>
-        </div>
+      window.addEventListener("click", goFaster);
+  })
+
   
-        <div className="scenery background1" ref={background}>
-          <img className="pawn" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright_small.png" alt=" " />
-          <img className="pawn2" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright_small.png" alt=" " />
-          <img className="treeback" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1_small.png" alt=" " />
-          <img id="w_rook" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_small.png" srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook.png 2x" alt=" " />
+  
+  return (
+    <div className="container">
+      <div className="sky"></div>
+      
+      <div className="earth">
+        <div className="alice">
+            <img className="alicesprite" ref={aliceSprite} src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png" alt=" " />
         </div>
       </div>
-    );
-  }
-  export default RedQueen;
+      
+      <div className="scenery" id="foreground" ref={foreground}>
+        <img id="treefore" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3_small.png" alt=" "/>
+      </div>
+
+      <div className="scenery background1" ref={background}>
+        <img className="pawn" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright_small.png" alt=" " />
+        <img className="pawn2" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/r_pawn_upright_small.png" alt=" " />
+        <img className="treeback" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm1_small.png" alt=" " />
+        <img id="w_rook" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook_small.png" srcset="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/w_rook.png 2x" alt=" " />
+      </div>
+    </div>
+  );
+}
+export default Sky;
