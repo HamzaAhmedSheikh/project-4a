173497e1f15cac6a7def05bc62d7d015f5685457
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-08-09 05:36:50 +0500

    lets check again

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import RedQueen from "./4A-RedQueen'sRace/App"
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <RedQueen />    
     </div>
   );
